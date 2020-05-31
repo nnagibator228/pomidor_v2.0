34a2859b528daa6b51f85d86a07774404f2d54ed
Arteom Zozulea <zmater2005@gmail.com>
2020-05-31 17:06:47 +0300

    something was fixed

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,28 +11,45 @@ import kotlin.concurrent.scheduleAtFixedRate
 
 class MainActivity : AppCompatActivity() {
     var a = 0
+
+    var curr = true
     var play1 = true
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var a1 = work.text.toString().toInt()*100+1;
+        var a2 = relax.text.toString().toInt()*100+1+a1;
+        var c =0
+        current.text = "$a:$a1:$a2"
         reset.setOnClickListener {
             play1 = false
             a = 0
         }
         play.setOnClickListener {
             play1 = !play1
-            current.text = "stopped"
         }
         val timer1 = Timer("schedule", true);
-        timer1.scheduleAtFixedRate(100, 100) {
+        timer1.scheduleAtFixedRate(10, 10) {
             if(play1) {
                 a += 1
             }
             this@MainActivity.runOnUiThread(java.lang.Runnable {
                 timer.text =  (a / 100).toString() + ":" +(a % 100).toString()
             })
+            if(a == a1 && curr){
+                c+=1
+                a1 = a+a2-c*work.text.toString().toInt()
+                current.text = "$a:$a1:$a2"
+                curr = !curr
+            }
+            if(a == a2 && !curr){
+                c+=1
+                a2 = a+a1-c*relax.text.toString().toInt()
+                current.text = "$a:$a1:$a2"
+                curr = !curr
+            }
         }
     }
 
